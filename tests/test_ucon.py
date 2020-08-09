1e73b04112f4cef5f4dd7fa48f612bcaec338252
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-08-09 00:25:14 -0500

    adds test_ucon.TestUnit.test___repr__

---
+++ tests/test_ucon.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from ucon import Unit
+
+
+class TestUnit(TestCase):
+
+    unit_name = 'second'
+    unit_aliases = ('seconds', 'secs', 's', 'S')
+    unit = Unit(unit_name, *unit_aliases)
+
+    def test___repr__(self):
+        self.assertEqual(f'<{self.unit_name}>', str(self.unit))
+
