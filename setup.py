e58acdba6affb23812376be50d3c8a70e3ad2076
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-09-07 14:42:13 -0500

    classifes project as "Alpha" release in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     maintainer_email='withtwoemms@gmail.com',
     url='https://github.com/withtwoemms/ucon',
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
