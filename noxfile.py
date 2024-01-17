70e3ec9a990d748fe7be2570198f1d7c1ca9bd48
Emmanuel I. Obi <withtwoemms@gmail.com>
2024-01-16 18:16:48 -0600

    updates noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,21 @@
 import nox
+import re
 
+from distutils.util import strtobool
+from subprocess import check_output
+from typing import List
 from os import environ as envvar
 
 
 PROJECT_NAME = 'ucon'
 VENV = f'{PROJECT_NAME}-venv'
-TESTDIR = '.'
 USEVENV = envvar.get('USEVENV', False)
+
+OFFICIAL = bool(strtobool(envvar.get('OFFICIAL', 'False')))
+COVERAGE = bool(strtobool(envvar.get('COVERAGE', 'True')))
+TESTDIR = f'tests.{PROJECT_NAME}'
+TESTNAME = envvar.get('TESTNAME', '')
+
 
 external = False if USEVENV else True
 supported_python_versions = [
@@ -19,33 +28,81 @@ supported_python_versions = [
 
 nox.options.default_venv_backend = 'none' if not USEVENV else USEVENV
 
+def session_name(suffix: str):
+    return f'{VENV}-{suffix}' if USEVENV else suffix
 
-@nox.session(name=VENV if USEVENV else PROJECT_NAME, python=supported_python_versions)
-def tests(session):
+
+def semver(version: str):
+    unofficial_semver = r'^([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?(.+)$'
+    official_semver = r'^([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'
+    _semver = re.search(official_semver, version) or re.search(unofficial_semver, version)
+    if _semver:
+        return [val for val in _semver.groups() if val]
+
+
+def is_official(semver: List[str]):
+    """
+    TODO (withtwoemms) -- create SemVer type to replace List[str]
+    """
+    if len(semver) > 3:
+        return False
+    else:
+        return True
+
+
+def latest_version(official: bool = False):
+    output = check_output('git for-each-ref --sort=creatordate --format %(refname) refs/tags'.split())
+    all_versions = (
+        version.lstrip('refs/tags/')
+        for version in reversed(output.decode().strip('\n').split('\n'))
+    )
+
+    if not official:
+        return next(all_versions)
+
+    for version in all_versions:
+        if official and is_official(semver(version)):
+            return version
+
+
+@nox.session(name=session_name('version'), python=supported_python_versions)
+def version(session):
+    print(latest_version(official=OFFICIAL))
+
+
+@nox.session(name=session_name('install'), python=supported_python_versions)
+def install(session):
     session.run(
         'python', '-m',
         'pip', '--disable-pip-version-check', 'install', '.',
         external=external
     )
-    session.run(
-        'python', '-m',
-        'pip', '--disable-pip-version-check', 'install', '-r', 'requirements.txt',
-        external=external
-    )
-    session.run(
-        'python', '-m',
-        'coverage', 'run', '--branch',
-        '--omit', '.nox/*,noxfile.py,setup.py,test*',
-        '--source', '.',
-        '-m', 'unittest', 'discover',
-        '-s', TESTDIR,
-        external=external
-    )
-    session.run('coverage', 'report', '-m', external=external)
-    session.run('coverage', 'xml', external=external)
 
 
-@nox.session(name=f'build-{PROJECT_NAME}')
+@nox.session(name=session_name('test'), python=supported_python_versions)
+def test(session):
+    if USEVENV:
+        install(session)
+
+    if COVERAGE:
+        session.run(
+            'python', '-m',
+            'coverage', 'run', '--source', '.', '--branch',
+            '--omit', '**tests/*,**/site-packages/*.py,noxfile.py,setup.py',
+            '-m', 'unittest', TESTNAME if TESTNAME else f'discover',
+            external=external
+        )
+        session.run('coverage', 'report', '-m', external=external)
+        session.run('coverage', 'xml', external=external)
+    else:
+        session.run(
+            'python', '-m',
+            'unittest', TESTNAME if TESTNAME else f'discover',
+            external=external
+        )
+
+
+@nox.session(name=f'build')
 def build(session):
     session.run('python', 'setup.py', 'sdist')
 
