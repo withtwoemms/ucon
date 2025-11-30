6312240e1a25c1af8e3fbb5a45566dbcc6964647
Emmanuel I. Obi <withtwoemms@gmail.com>
2025-11-29 22:45:47 -0600

    adds copyright headers to all test modules

---
+++ tests/ucon/__init__.py
@@ -0,0 +1,3 @@
+# © 2025 The Radiativity Company
+# Licensed under the Apache License, Version 2.0
+# See the LICENSE file for details.
--- tests/ucon/test_algebra.py
+++ tests/ucon/test_algebra.py
@@ -1,4 +1,6 @@
-
+# © 2025 The Radiativity Company
+# Licensed under the Apache License, Version 2.0
+# See the LICENSE file for details.
 
 import math
 from unittest import TestCase
