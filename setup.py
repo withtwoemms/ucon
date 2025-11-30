876fceb34f4e93d4786293463a91bd908174c647
Emmanuel I. Obi <withtwoemms@gmail.com>
2025-11-29 23:22:30 -0600

    adds copyright headers to all distributed modules

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# © 2025 The Radiativity Company
+# Licensed under the Apache License, Version 2.0
+# See the LICENSE file for details.
+
 from os import environ as envvars
 from pathlib import Path
 from setuptools import find_packages, setup
