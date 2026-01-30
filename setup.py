f507ef4ad5b17cefb6169595b3fccf9baac4b6c7
Emmanuel I. Obi <withtwoemms@gmail.com>
2026-01-30 15:46:36 -0600

    upgrades to beta release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     maintainer_email='withtwoemms@gmail.com',
     url='https://github.com/withtwoemms/ucon',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
