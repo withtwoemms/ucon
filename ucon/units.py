6b7fffa71e92549da29c05e50fc44aca9784e47b
Emmanuel I. Obi <withtwoemms@gmail.com>
2026-01-30 02:51:03 -0600

    ucon.units entries make use of the new Unit dataclass

--- ucon/units.py
+++ ucon/units.py
@@ -35,38 +35,75 @@ none = Unit()
 
 
 # -- International System of Units (SI) --------------------------------
-ampere = Unit('I', 'amp', name='ampere', dimension=Dimension.current)
-becquerel = Unit('Bq', name='becquerel', dimension=Dimension.frequency)
-celsius = Unit('°C', name='celsius', dimension=Dimension.temperature)
-coulomb = Unit('C', name='coulomb', dimension=Dimension.charge)
-farad = Unit('F', name='farad', dimension=Dimension.capacitance)
-gram = Unit('g', 'G', name='gram', dimension=Dimension.mass)
-gray = Unit('Gy', name='gray', dimension=Dimension.energy)
-henry = Unit('H', name='henry', dimension=Dimension.inductance)
-hertz = Unit('Hz', name='hertz', dimension=Dimension.frequency)
-hour = Unit('h', 'H', name='hour', dimension=Dimension.time)
-joule = Unit('J', name='joule', dimension=Dimension.energy)
-joule_per_kelvin = Unit('J/K', name='joule_per_kelvin', dimension=Dimension.entropy)
-kelvin = Unit('K', name='kelvin', dimension=Dimension.temperature)
-liter = Unit('L', 'l', name='liter', dimension=Dimension.volume)
-lumen = Unit('lm', name='lumen', dimension=Dimension.luminous_intensity)
-lux = Unit('lx', name='lux', dimension=Dimension.illuminance)
-meter = Unit('m', 'M', name='meter', dimension=Dimension.length)
-mole = Unit('mol', 'n', name='mole', dimension=Dimension.amount_of_substance)
-newton = Unit('N', name='newton', dimension=Dimension.force)
-ohm = Unit('Ω', name='ohm', dimension=Dimension.resistance)
-pascal = Unit('Pa', name='pascal', dimension=Dimension.pressure)
-radian = Unit('rad', name='radian', dimension=Dimension.none)
-second = Unit('s', 'sec', name='second', dimension=Dimension.time)
-sievert = Unit('Sv', name='sievert', dimension=Dimension.energy)
-siemens = Unit('S', name='siemens', dimension=Dimension.conductance)
-steradian = Unit('sr', name='steradian', dimension=Dimension.none)
-tesla = Unit('T', name='tesla', dimension=Dimension.magnetic_flux_density)
-volt = Unit('V', name='volt', dimension=Dimension.voltage)
-watt = Unit('W', name='watt', dimension=Dimension.power)
-webers = Unit('Wb', name='weber', dimension=Dimension.magnetic_flux)
-webers_per_meter = Unit('Wb/m', name='webers_per_meter', dimension=Dimension.magnetic_permeability)
+ampere = Unit(name='ampere', dimension=Dimension.current, aliases=('I', 'amp'))
+becquerel = Unit(name='becquerel', dimension=Dimension.frequency, aliases=('Bq',))
+celsius = Unit(name='celsius', dimension=Dimension.temperature, aliases=('°C', 'degC'))
+coulomb = Unit(name='coulomb', dimension=Dimension.charge, aliases=('C',))
+farad = Unit(name='farad', dimension=Dimension.capacitance, aliases=('F',))
+gram = Unit(name='gram', dimension=Dimension.mass, aliases=('g',))
+gray = Unit(name='gray', dimension=Dimension.energy, aliases=('Gy',))
+henry = Unit(name='henry', dimension=Dimension.inductance, aliases=('H',))
+hertz = Unit(name='hertz', dimension=Dimension.frequency, aliases=('Hz',))
+joule = Unit(name='joule', dimension=Dimension.energy, aliases=('J',))
+joule_per_kelvin = Unit(name='joule_per_kelvin', dimension=Dimension.entropy, aliases=('J/K',))
+kelvin = Unit(name='kelvin', dimension=Dimension.temperature, aliases=('K',))
+kilogram = Unit(name='kilogram', dimension=Dimension.mass, aliases=('kg',))
+liter = Unit(name='liter', dimension=Dimension.volume, aliases=('L', 'l'))
+lumen = Unit(name='lumen', dimension=Dimension.luminous_intensity, aliases=('lm',))
+lux = Unit(name='lux', dimension=Dimension.illuminance, aliases=('lx',))
+meter = Unit(name='meter', dimension=Dimension.length, aliases=('m',))
+mole = Unit(name='mole', dimension=Dimension.amount_of_substance, aliases=('mol', 'n'))
+newton = Unit(name='newton', dimension=Dimension.force, aliases=('N',))
+ohm = Unit(name='ohm', dimension=Dimension.resistance, aliases=('Ω',))
+pascal = Unit(name='pascal', dimension=Dimension.pressure, aliases=('Pa',))
+radian = Unit(name='radian', dimension=Dimension.none, aliases=('rad',))
+siemens = Unit(name='siemens', dimension=Dimension.conductance, aliases=('S',))
+sievert = Unit(name='sievert', dimension=Dimension.energy, aliases=('Sv',))
+steradian = Unit(name='steradian', dimension=Dimension.none, aliases=('sr',))
+tesla = Unit(name='tesla', dimension=Dimension.magnetic_flux_density, aliases=('T',))
+volt = Unit(name='volt', dimension=Dimension.voltage, aliases=('V',))
+watt = Unit(name='watt', dimension=Dimension.power, aliases=('W',))
+weber = Unit(name='weber', dimension=Dimension.magnetic_flux, aliases=('Wb',))
+webers_per_meter = Unit(name='webers_per_meter', dimension=Dimension.magnetic_permeability, aliases=('Wb/m',))
 # ----------------------------------------------------------------------
+
+
+# -- Time Units --------------------------------------------------------
+second = Unit(name='second', dimension=Dimension.time, aliases=('s', 'sec'))
+minute = Unit(name='minute', dimension=Dimension.time, aliases=('min',))
+hour = Unit(name='hour', dimension=Dimension.time, aliases=('h', 'hr'))
+day = Unit(name='day', dimension=Dimension.time, aliases=('d',))
+# ----------------------------------------------------------------------
+
+
+# -- Imperial / US Customary Units -------------------------------------
+# Length
+foot = Unit(name='foot', dimension=Dimension.length, aliases=('ft',))
+inch = Unit(name='inch', dimension=Dimension.length, aliases=('in',))
+yard = Unit(name='yard', dimension=Dimension.length, aliases=('yd',))
+mile = Unit(name='mile', dimension=Dimension.length, aliases=('mi',))
+
+# Mass
+pound = Unit(name='pound', dimension=Dimension.mass, aliases=('lb', 'lbs'))
+ounce = Unit(name='ounce', dimension=Dimension.mass, aliases=('oz',))
+
+# Temperature
+fahrenheit = Unit(name='fahrenheit', dimension=Dimension.temperature, aliases=('°F', 'degF'))
+
+# Volume
+gallon = Unit(name='gallon', dimension=Dimension.volume, aliases=('gal',))
+
+# Energy
+calorie = Unit(name='calorie', dimension=Dimension.energy, aliases=('cal',))
+btu = Unit(name='btu', dimension=Dimension.energy, aliases=('BTU',))
+
+# Power
+horsepower = Unit(name='horsepower', dimension=Dimension.power, aliases=('hp',))
+# ----------------------------------------------------------------------
+
+
+# Backward compatibility alias
+webers = weber
 
 
 def have(name: str) -> bool:
